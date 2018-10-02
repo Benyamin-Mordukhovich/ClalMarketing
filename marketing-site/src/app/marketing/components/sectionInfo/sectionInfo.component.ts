a87f7972357a3edd49eb57ec39eccee32b4e4331
Benyamin Mordukhovich <Benyamin.Mordukhovich@Giant-Steps.local>
2018-10-02 12:02:22 +0300

    adding opacity and scale controll with scrolling

--- marketing-site/src/app/marketing/components/sectionInfo/sectionInfo.component.ts
+++ marketing-site/src/app/marketing/components/sectionInfo/sectionInfo.component.ts
@@ -4,11 +4,53 @@ import { Component, Input } from '@angular/core';
     templateUrl: './sectionInfo.component.html'
 })
 export class SectionInfoComponent {
-
     @Input() infoObj;
 
-    constructor() {
+    private _scrollTop: number = 0;
+    public lastScrollTop: number = 0;
+    public opacityValue: number = 1;
+    public scaleValue: number = 1;
+    public visibilityValue: string = 'visible';
+
+    @Input()
+    set scrollTop(scrollTop: number) {
+        this._scrollTop = scrollTop || 0;
+        this.opacityValue = getCssValue(this._scrollTop, this.lastScrollTop, 'decrease', this.infoObj.scrollBegin, this.infoObj.scrollEnd) / 100;
+        this.scaleValue = 1 + getCssValue(this._scrollTop, this.lastScrollTop, 'increase', this.infoObj.scrollBegin, this.infoObj.scrollEnd) / 100;
+        this.lastScrollTop = this._scrollTop <= 0 ? 0 : this._scrollTop;
     }
 
+    get scrollTop(): number { return this._scrollTop; }
 
+    constructor() { }
+}
+
+function getCssValue(st: number, lastScrollTop: number, cssValueDir: string, scrollBegin: number, scrollEnd: number): number {
+    let percentage;
+
+    if (st > lastScrollTop) {
+        //console.log("scroll down");
+        if(st > scrollEnd) {
+            console.log("hidden");
+            this.visibilityValue = "hidden";
+        }
+    } else {
+        //console.log("scroll up");
+        if(st < scrollEnd) {
+            console.log("visible");
+            this.visibilityValue = "visible";
+        }
+    }
+
+    if (st < scrollBegin) return;
+
+    if (cssValueDir == "decrease") {
+        percentage = 100 - ((st - scrollBegin) / ((scrollEnd - scrollBegin) / 100));                
+        if(percentage <= 0) return;
+    } else {
+        percentage = ((st - scrollBegin) / ((scrollEnd - scrollBegin) / 100));
+        if(percentage >= 100) return;
+    }
+
+    return percentage;
 }
--- marketing-site/src/app/marketing/containers/hp/hp.component.ts
+++ marketing-site/src/app/marketing/containers/hp/hp.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription, fromEvent, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { DataService } from '../../../services/data.service';
 
 @Component({
@@ -6,8 +8,13 @@ import { DataService } from '../../../services/data.service';
   templateUrl: './hp.component.html'
 })
 export class HpComponent implements OnInit {
+  scrollTop: number;
+  hpHeight: number = 4000;
+  opacityValue: number = 1;
+  lastScrollTop: number = 0;
 
-  private sectionItems:any = [];
+  private sectionItems: any = [];
+
   constructor(private _dataService: DataService) {
 
   }
@@ -15,12 +22,27 @@ export class HpComponent implements OnInit {
   ngOnInit() {
     this._dataService.getHomePage().subscribe(
       res => {
-        this.sectionItems = res
+        this.sectionItems = res;
+
+        let interval = this.hpHeight / this.sectionItems.length;
+        let distance = 0;
+
+        this.sectionItems.forEach(e => {
+          e.scrollBegin = distance;
+          e.scrollEnd = distance + interval;
+          distance += interval;
+        });
+
         console.log("res",this.sectionItems)
       }
     )
-  }
 
+    //create observable that emits click events
+    const source = fromEvent(window, 'scroll').pipe(map(e => e));
+    source.subscribe(val => {
+      this.scrollTop = window.pageYOffset;
+    });
+  }
   // sectionInfoVideo = {
   //   mainTitle: "לעבוד בראש שקט<br>עם ביטוח אשראי מבית כלל",
   //   text: "לראשונה בישראל, שירותי ביטוח אשראי של כלל ניתנים גם לעסקים בינוניים וקטנים,בעלי מחזור הכנסות שנתי שבין 10-50 מיליון שח ביטוח אשראי מאפשר לך לבצע יותר עסקאות בצורה בטוחה,ומקנה ייתרון במימון הבנקאי כל זאת עם גב של חברת כלל ביטוח",
