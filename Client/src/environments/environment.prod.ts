9e5b45ac4f0cbbc0faedb1e416bdd75688ec5d26
Shai.Matsri <Shai.Matsri@SHAI_MA-PC>
2018-11-05 10:50:09 +0200

    שדג

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 import { ContentUrls } from "../types";
 
 const urls: ContentUrls = {
-  hp: 'http://dev.clalmarketing.com/umbraco/api/app/Content?url=/',
-  about: "http://dev.clalmarketing.com/umbraco/api/app/Content?url=/about",
-  contact: "http://dev.clalmarketing.com/umbraco/api/app/Content?url=/contact",
-  faq: "http://dev.clalmarketing.com/umbraco/api/app/Content?url=/faq",
-  contactSubmitUrl: "http://dev.clalmarketing.com/umbraco/api/Contact/Submit/"
+  hp: 'http://bo.clalmarketing.com/umbraco/api/app/Content?url=/',
+  about: "http://bo.clalmarketing.com/umbraco/api/app/Content?url=/about",
+  contact: "http://bo.clalmarketing.com/umbraco/api/app/Content?url=/contact",
+  faq: "http://bo.clalmarketing.com/umbraco/api/app/Content?url=/faq",
+  contactSubmitUrl: "http://bo.clalmarketing.com/umbraco/api/Contact/Submit/"
 }
 
 export const environment = {
