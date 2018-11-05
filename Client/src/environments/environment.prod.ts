4ad9bc813ff1a18919278f52fbf7615a6bec3642
Shai.Matsri <Shai.Matsri@SHAI_MA-PC>
2018-11-05 17:12:19 +0200

    ficed prod build

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 import { ContentUrls } from "../types";
 
 const urls: ContentUrls = {
-  hp: 'http://callcci.stanga.co.il/umbraco/api/app/Content?url=/',
-  about: "http://callcci.stanga.co.il/umbraco/api/app/Content?url=/about",
-  contact: "http://callcci.stanga.co.il/umbraco/api/app/Content?url=/contact",
-  faq: "http://callcci.stanga.co.il/umbraco/api/app/Content?url=/faq",
-  contactSubmitUrl: "http://callcci.stanga.co.il/umbraco/api/Contact/Submit/"
+  hp: 'http://clalsmbbackoffice.stanga.co.il/umbraco/api/app/Content?url=/',
+  about: "http://clalsmbbackoffice.stanga.co.il/umbraco/api/app/Content?url=/about",
+  contact: "http://clalsmbbackoffice.stanga.co.il/umbraco/api/app/Content?url=/contact",
+  faq: "http://clalsmbbackoffice.stanga.co.il/umbraco/api/app/Content?url=/faq",
+  contactSubmitUrl: "http://clalsmbbackoffice.stanga.co.il/umbraco/api/Contact/Submit/"
 }
 
 export const environment = {
