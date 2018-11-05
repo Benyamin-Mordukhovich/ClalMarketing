30e8ef24daa6dd7b584cb9d78f6a20cc7a93f44b
Shai.Matsri <Shai.Matsri@SHAI_MA-PC>
2018-11-05 12:59:39 +0200

    configure stanga

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 import { ContentUrls } from "../types";
 
 const urls: ContentUrls = {
-  hp: 'http://bo.clalmarketing.com/umbraco/api/app/Content?url=/',
-  about: "http://bo.clalmarketing.com/umbraco/api/app/Content?url=/about",
-  contact: "http://bo.clalmarketing.com/umbraco/api/app/Content?url=/contact",
-  faq: "http://bo.clalmarketing.com/umbraco/api/app/Content?url=/faq",
-  contactSubmitUrl: "http://bo.clalmarketing.com/umbraco/api/Contact/Submit/"
+  hp: 'http://callcci.stanga.co.il/umbraco/api/app/Content?url=/',
+  about: "http://callcci.stanga.co.il/umbraco/api/app/Content?url=/about",
+  contact: "http://callcci.stanga.co.il/umbraco/api/app/Content?url=/contact",
+  faq: "http://callcci.stanga.co.il/umbraco/api/app/Content?url=/faq",
+  contactSubmitUrl: "http://callcci.stanga.co.il/umbraco/api/Contact/Submit/"
 }
 
 export const environment = {
