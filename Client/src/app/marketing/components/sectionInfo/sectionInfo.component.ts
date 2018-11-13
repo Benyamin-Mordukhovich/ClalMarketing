4af0482a2148a2af849251a861a35becedf522fe
Shai.Matsri <Shai.Matsri@SHAI_MA-PC>
2018-11-13 12:55:46 +0200

    add pause video on scrolltop

--- Client/src/app/marketing/components/sectionInfo/sectionInfo.component.ts
+++ Client/src/app/marketing/components/sectionInfo/sectionInfo.component.ts
@@ -1,28 +1,54 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnInit } from "@angular/core";
 @Component({
-    selector: 'app-sectionInfo',
-    templateUrl: './sectionInfo.component.html'
+  selector: "app-sectionInfo",
+  templateUrl: "./sectionInfo.component.html"
 })
-export class SectionInfoComponent   {
-    @Input() infoObj;
-    @ViewChild('videoRef') videoElementRef: ElementRef;
-    isVideoPlaying:boolean = false;
-    constructor() { }
-
-    playVideo(){
-        if (this.infoObj.videoUrl.length ){
-            if(this.isVideoPlaying == false){
-                this.videoElementRef.nativeElement.play();
-                this.isVideoPlaying = true;
-            }
-        }
+export class SectionInfoComponent implements OnInit {
+  @Input() infoObj;
+  @Input() isMobile;
+  @Input()
+  set scrollTop(scrollTop: number) {
+    if (scrollTop >= 500) {
+      this.stopVideo();
     }
+  }
 
-    stopVideo(){
-        if(this.isVideoPlaying == true){
-            this.videoElementRef.nativeElement.pause();
-            this.isVideoPlaying = false;
-        }
+  @ViewChild("videoRef") videoElementRef: ElementRef;
+
+  isVideoPlaying: boolean = false;
+  constructor() {}
+
+  playVideo() {
+    if (this.infoObj.videoUrl.length) {
+      if (this.isVideoPlaying == false) {
+        this.videoElementRef.nativeElement.play();
+        this.isVideoPlaying = true;
+      }
     }
+  }
 
+  stopVideo() {
+    if (this.isVideoPlaying == true) {
+      this.videoElementRef.nativeElement.pause();
+      this.isVideoPlaying = false;
+    }
+  }
+
+  ngOnInit() {
+    if (!this.isMobile) {
+      setTimeout(() => {
+        let videoBgs = [
+          document.querySelectorAll("video")[1],
+          document.querySelectorAll("video")[2],
+          document.querySelectorAll("video")[3],
+          document.querySelectorAll("video")[4]
+        ];
+
+        videoBgs.forEach(video => {
+          video.muted = true;
+          video.play();
+        });
+      }, 0);
+    }
+  }
 }
