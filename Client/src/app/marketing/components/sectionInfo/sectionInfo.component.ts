a826960725ec4a9b57a103bb6c2cd37ac38b2975
galit.shaul <galit.shaul@ngsoft.com>
2018-11-13 11:51:50 +0200

    add pause video on scrolltop

--- Client/src/app/marketing/components/sectionInfo/sectionInfo.component.ts
+++ Client/src/app/marketing/components/sectionInfo/sectionInfo.component.ts
@@ -6,39 +6,45 @@ import { Component, Input, ViewChild, ElementRef, OnInit } from '@angular/core';
 export class SectionInfoComponent implements OnInit {
     @Input() infoObj;
     @Input() isMobile;
+    @Input()
+    set scrollTop(scrollTop: number) {
+        if (scrollTop >= 500) {
+            this.stopVideo();
+        }
+    }
 
     @ViewChild('videoRef') videoElementRef: ElementRef;
 
-    isVideoPlaying:boolean = false;
+    isVideoPlaying: boolean = false;
     constructor() { }
 
-    playVideo(){
-        if (this.infoObj.videoUrl.length ) {
-            if(this.isVideoPlaying == false){
+    playVideo() {
+        if (this.infoObj.videoUrl.length) {
+            if (this.isVideoPlaying == false) {
                 this.videoElementRef.nativeElement.play();
                 this.isVideoPlaying = true;
             }
         }
     }
 
-    stopVideo(){
-        if(this.isVideoPlaying == true){
+    stopVideo() {
+        if (this.isVideoPlaying == true) {
             this.videoElementRef.nativeElement.pause();
             this.isVideoPlaying = false;
         }
     }
 
     ngOnInit() {
-        if(!this.isMobile) {
-            setTimeout(()=> {
+        if (!this.isMobile) {
+            setTimeout(() => {
                 let videoBgs = [
                     document.querySelectorAll('video')[1],
                     document.querySelectorAll('video')[2],
                     document.querySelectorAll('video')[3],
                     document.querySelectorAll('video')[4]
                 ]
-    
-                videoBgs.forEach( video => {
+
+                videoBgs.forEach(video => {
                     video.muted = true;
                     video.play();
                 })
